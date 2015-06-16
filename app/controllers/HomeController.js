8d9fb4da5bb560035f2d8de6fa1a69ce876ffc95
Steve Fabian <steve.fabian@gooddogs.com>
2015-06-16 13:32:42 -0400

    code cleanup
    some minor code cleanup

--- app/controllers/HomeController.js
+++ app/controllers/HomeController.js
@@ -6,7 +6,9 @@
 
         $scope.message = "Hello from the Home Controller file";
 
-    };
+        $scope.title = "This is the Home Controller";
+
+    }
 
     app.controller('HomeController', HomeController);
 
